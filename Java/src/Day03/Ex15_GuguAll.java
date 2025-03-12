093f423a19d53f16af99dedda4ccbb8ac09ae10f
tj-bu-706-09 <tj-bu-706-09@DESKTOP-MMU6BKK>
2025-03-12 18:07:28 +0900

    3일차

---
+++ Java/src/Day03/Ex15_GuguAll.java
@@ -0,0 +1,24 @@
+package Day03;
+
+import java.util.Scanner;
+
+public class Ex15_GuguAll {
+   
+	 public static void main(String[] args) {
+		// (단)x(1~9)
+		// 단에 대한 반복 : 1~9  : j
+		// 각 단의 곱 : 1~9    : i
+		 
+		// 중첩 반복문(이중 반복문) 
+		// * 안쪽 반복문이 먼저 다 돌고, 바깥쪽 반복문의 반복변수가 증감된다. 
+		Scanner sc = new Scanner(System.in);
+		for (int i = 1; i <= 9; i++) {
+			
+			for (int j = 1; j <=9; j++) {
+				System.out.print(j + "x" + i + "=" + (i*j));
+				System.out.print("\t");       // 탭(공백)
+			}
+			System.out.println();
+		}
+	}
+}
